6d1c72715d146fb4e6b5236bfd6704ba4c55ddeb
Omar Sulca <omarsulca@gmail.com>
2022-02-10 10:55:57 -0500

    api peliculas funcional

---
+++ app/Http/Controllers/PeliculaController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelicula;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+
+class PeliculaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $result = Pelicula::all();
+        return response($result, 201);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Pelicula::create($request->all());
+        return response(
+            ["mensaje" => "exito"]
+            ,201
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pelicula $pelicula)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pelicula $pelicula)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, int $pelicula)
+    {
+        $pelicula = Pelicula::find($pelicula);
+        $pelicula->update($request->all());
+        return response($pelicula,201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $pelicula)
+    {
+        $pelicula = Pelicula::find($pelicula);
+        $pelicula->delete();
+        return response(
+            ["mensaje" => "eliminado"]
+            ,201
+        );
+    }
+}
---
+++ database/migrations/2022_02_10_151238_create_peliculas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeliculasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->id();
+            $table->string("titulo");
+            $table->integer("año");
+            $table->string("genero");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peliculas');
+    }
+}
